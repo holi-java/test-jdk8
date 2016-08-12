52e46e62c61d4b25ce56a75156a48651b65d1255
selonj <selonj@126.com>
2016-08-13 04:36:40 +0800

    test catch with multi exceptions

---
+++ src/test/java/com/holi/CatchMultiExceptionsTest.java
@@ -0,0 +1,39 @@
+package com.holi;
+
+import java.io.IOException;
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.sameInstance;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+
+/**
+ * Created by selonj on 16-8-13.
+ */
+public class CatchMultiExceptionsTest {
+
+  @Test public void couldCaughtMultiExceptions() throws Exception {
+    assertExceptionCouldBeCaught(new IllegalArgumentException());
+    assertExceptionCouldBeCaught(new ArithmeticException());
+
+    assertExceptionMissingCaught(new IOException());
+  }
+
+  private void assertExceptionMissingCaught(Exception expected) throws Exception {
+    try {
+      assertExceptionCouldBeCaught(expected);
+    } catch (Exception actual) {
+      if (expected == actual) return;
+    }
+    fail(String.format("should %s missing caught!", expected.getClass().getName()));
+  }
+
+  private void assertExceptionCouldBeCaught(Exception expected) throws Exception {
+    try {
+      throw expected;
+    } catch (IllegalArgumentException | ArithmeticException actual) {
+      assertThat(actual, is(sameInstance(expected)));
+    }
+  }
+}
